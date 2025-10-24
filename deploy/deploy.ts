fbf117b4c56e94ed682fce46ef0829ce74ed7b3a
r <50800165+raj-bhadra@users.noreply.github.com>
2025-10-24 23:29:13 +0530

    Add deployment code for all contracts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -11,7 +11,36 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   });
 
   console.log(`FHECounter contract: `, deployedFHECounter.address);
+
+  const deployedUsdc = await deploy("USDC", {
+    from: deployer,
+    log: true,
+  });
+
+  console.log(`USDC contract: `, deployedUsdc.address);
+
+  const deployedConfidentialTokenWrapper = await deploy("ConfidentialTokenWrapper", {
+    from: deployer,
+    args: [deployedUsdc.address],
+    log: true,
+  });
+
+  console.log(`ConfidentialTokenWrapper contract: `, deployedConfidentialTokenWrapper.address);
+
+  const deployedConfidentialTokenFactory = await deploy("ConfidentialTokenFactory", {
+    from: deployer,
+    log: true,
+  });
+
+  console.log(`ConfidentialTokenFactory contract: `, deployedConfidentialTokenFactory.address);
+
+  const deployedZBondingCurve = await deploy("ZBondingCurve", {
+    from: deployer,
+    args: [deployedConfidentialTokenWrapper.address, deployedConfidentialTokenFactory.address],
+    log: true,
+  });
+  console.log(`ZBondingCurve contract: `, deployedZBondingCurve.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_assets"; // id required to prevent reexecution
+func.tags = ["FHECounter", "USDC", "ConfidentialTokenWrapper", "ConfidentialTokenFactory", "ZBondingCurve"];
