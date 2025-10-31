b8dd71eadbb1bd3ce3dcf730e9bb429859e00bb4
r <50800165+raj-bhadra@users.noreply.github.com>
2025-10-31 10:41:09 +0530

    Add token info with paginated gets

--- contracts/ConfidentialTokenFactory.sol
+++ contracts/ConfidentialTokenFactory.sol
@@ -10,11 +10,29 @@ contract ConfidentialTokenFactory is SepoliaConfig {
     address[] public _tokenAddresses;
     event TokenCreated(address indexed tokenAddress);
     mapping(address => bool) public isTokenRegistered;
+    struct TokenInfo {
+        address createdBy;
+        string name;
+        string symbol;
+        string contractURI;
+        address tokenAddress;
+    }
+    TokenInfo[] public tokenInfos;
+    mapping(address => TokenInfo[]) public tokenInfosByCreatorAddress;
 
     function createToken(string memory name, string memory symbol, string memory contractURI) external {
         ConfidentialToken token = new ConfidentialToken(name, symbol, contractURI);
         _tokenAddresses.push(address(token));
         isTokenRegistered[address(token)] = true;
+        tokenInfos.push(
+            TokenInfo({
+                createdBy: msg.sender,
+                name: name,
+                symbol: symbol,
+                contractURI: contractURI,
+                tokenAddress: address(token)
+            })
+        );
         emit TokenCreated(address(token));
     }
 
@@ -24,5 +42,42 @@ contract ConfidentialTokenFactory is SepoliaConfig {
 
     function isConfidentialTokenRegistered(address tokenAddress) external view returns (bool) {
         return isTokenRegistered[tokenAddress];
+    }
+
+    function getTokenCount() external view returns (uint256) {
+        return tokenInfos.length;
+    }
+
+    function getTokenInfosCountByCreator(address creatorAddress) external view returns (uint256) {
+        return tokenInfosByCreatorAddress[creatorAddress].length;
+    }
+    function getPaginatedTokenInfos(uint256 page, uint256 pageSize) external view returns (TokenInfo[] memory) {
+        uint256 startIndex = page * pageSize;
+        uint256 endIndex = startIndex + pageSize;
+        if (endIndex > tokenInfos.length) {
+            endIndex = tokenInfos.length;
+        }
+        TokenInfo[] memory paginatedTokenInfos = new TokenInfo[](endIndex - startIndex);
+        for (uint256 i = startIndex; i < endIndex; i++) {
+            paginatedTokenInfos[i - startIndex] = tokenInfos[i];
+        }
+        return paginatedTokenInfos;
+    }
+
+    function getPaginatedTokenInfosByCreator(
+        address creatorAddress,
+        uint256 page,
+        uint256 pageSize
+    ) external view returns (TokenInfo[] memory) {
+        uint256 startIndex = page * pageSize;
+        uint256 endIndex = startIndex + pageSize;
+        if (endIndex > tokenInfosByCreatorAddress[creatorAddress].length) {
+            endIndex = tokenInfosByCreatorAddress[creatorAddress].length;
+        }
+        TokenInfo[] memory paginatedTokenInfos = new TokenInfo[](endIndex - startIndex);
+        for (uint256 i = startIndex; i < endIndex; i++) {
+            paginatedTokenInfos[i - startIndex] = tokenInfosByCreatorAddress[creatorAddress][i];
+        }
+        return paginatedTokenInfos;
     }
 }
